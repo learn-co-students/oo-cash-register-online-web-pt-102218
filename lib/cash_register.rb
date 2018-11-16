8f38e5c3e160d56170e677ee8ce8a9f6ae96865d
Linda Chang <lindachang186@gmail.com>
2018-11-16 16:20:51 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+require "pry"
 
+class CashRegister
+  attr_accessor :total, :discount
+
+  @@basket = Hash.new
+
+  def initialize(total = 0, discount = 20)
+    @total = total
+    @discount = discount
+  end
+
+def total
+  @total
+end
+
+def add_item(title, price, quantity = 1)
+  @@basket[title] = [price, quantity]
+    @@basket.each do |item, value|
+      if quantity > 1
+      @total = total + value.inject(:*)
+      end
+    end
+end
+
+
+
+
+end
