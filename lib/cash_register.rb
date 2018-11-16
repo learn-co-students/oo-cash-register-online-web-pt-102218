252c28e98336cc662a7afea769a1f436eaecf78c
Linda Chang <lindachang186@gmail.com>
2018-11-16 16:45:49 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,28 +1,23 @@
 require "pry"
 
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items
 
-  @@basket = Hash.new
+
 
   def initialize(total = 0, discount = 20)
     @total = total
     @discount = discount
+    @items = []
   end
 
-def total
-  @total
-end
 
-def add_item(title, price, quantity = 1)
-  @@basket[title] = [price, quantity]
-    @@basket.each do |item, value|
-      if quantity > 1
-      @total = total + value.inject(:*)
-      end
-    end
+def add_item(title, price, quantity=1)
+  self.total += price * quantity
+  #  quantity.times do
+    #  @items << title
+#    end
 end
-
 
 
 
