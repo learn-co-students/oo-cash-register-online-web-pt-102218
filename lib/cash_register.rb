428cafeacb3c8020c122513026f5880244947a1f
Johnathon Wood <jaywood128@gmail.com>
2018-11-19 09:44:48 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+require 'pry'
+class CashRegister
 
+attr_accessor :total, :employee_discount, :title, :price
+
+
+  def initialize(employee_discount = 0)
+    @total = 0.00
+    @employee_discount = employee_discount
+    @items = []
+  end
+
+  def discount
+    self.employee_discount
+  end
+
+  def add_item(title, price, quanitity = 1)
+    self.total += price *quanitity
+    quanitity.times do
+      @items << title
+    end
+  end
+
+  def apply_discount
+
+    amount_discounted = @total * (employee_discount/100.0)
+    self.total = @total - amount_discounted
+   "After the discount, the total comes to $#{self.total}"
+  end
+
+end
